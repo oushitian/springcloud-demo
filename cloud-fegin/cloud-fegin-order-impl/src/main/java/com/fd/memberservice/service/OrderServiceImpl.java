730093e668733ed4057878ca74b8023ab0c9c407
fengdi <Fd88025442>
2018-09-26 14:36:50 +0800

    zuul的路由及过滤功能

--- cloud-fegin/cloud-fegin-order-impl/src/main/java/com/fd/memberservice/service/OrderServiceImpl.java
+++ cloud-fegin/cloud-fegin-order-impl/src/main/java/com/fd/memberservice/service/OrderServiceImpl.java
@@ -1,14 +1,16 @@
 package com.fd.memberservice.service;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @Author xyl
  * @Create 2018-09-25 9:11
  * @Desc 写点注释吧
  **/
-@Service
+@RestController
 public class OrderServiceImpl implements OrderService{
 
     @RequestMapping("/getOrder")
--- cloud-zuul/src/main/java/com/fd/cloudzuul/CloudZuulApplication.java
+++ cloud-zuul/src/main/java/com/fd/cloudzuul/CloudZuulApplication.java
@@ -2,8 +2,12 @@ package com.fd.cloudzuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy		//声明zuul
+@EnableEurekaClient		//注册到注册中心
 public class CloudZuulApplication {
 
 	public static void main(String[] args) {
